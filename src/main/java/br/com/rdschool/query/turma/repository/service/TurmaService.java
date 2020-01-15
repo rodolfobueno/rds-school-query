cbef58fc5c4acd314cfacc73da6b3495494aeaea
Rodolfo <rodolfo.bueno13@gmail.com>
2020-01-15 18:14:17 -0300

    Realizando persistência de uma turma com os relacionamentos de alunos e disciplinas.

--- src/main/java/br/com/rdschool/query/turma/repository/service/TurmaService.java
+++ src/main/java/br/com/rdschool/query/turma/repository/service/TurmaService.java
@@ -1,12 +1,17 @@
 package br.com.rdschool.query.turma.repository.service;
 
+import java.util.Collection;
+import java.util.HashSet;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.rdschool.query.turma.amqp.events.TurmaCriadaEvent;
-import br.com.rdschool.query.turma.repository.TurmaModel;
+import br.com.rdschool.query.turma.repository.Turma;
+import br.com.rdschool.query.turma.repository.TurmaAluno;
+import br.com.rdschool.query.turma.repository.TurmaDisciplina;
 import br.com.rdschool.query.turma.repository.TurmaRepository;
 
 @Service
@@ -17,13 +22,22 @@ public class TurmaService {
 	private TurmaRepository repository;
 	
 	public void handle(TurmaCriadaEvent event) {
-		TurmaModel turma = TurmaModel.builder()
+		Collection<TurmaAluno> alunos = new HashSet<>();
+		Collection<TurmaDisciplina> disciplinas = new HashSet<>();
+		
+		event.getAlunosId().forEach(aluno -> alunos.add(TurmaAluno.of(aluno.getId())));
+		event.getDisciplinasId().forEach(disc -> disciplinas.add(TurmaDisciplina.of(disc.getId())));
+
+		Turma turma = Turma.builder()
 				.id(event.getTurmaId())
 				.descricao(event.getDescricao())
 				.anoLetivo(event.getAnoLetivo())
 				.periodoLetivo(event.getPeriodoLetivo())
 				.numeroVagas(event.getNumeroVagas())
+				.alunos(alunos)
+				.disciplinas(disciplinas)
 				.build();
+
 		repository.save(turma);
 	}
 }
